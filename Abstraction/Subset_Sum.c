9b3e276f0ffbd28abf8e2f0731a161b02ba7140d
Garrett Goode <ggoode@harvardapp.com>
2017-01-25 07:49:10 -0500

    Initial commit of subset sub module
    Just some stubs for now, still testing completed version.

---
+++ Abstraction/Subset_Sum.c
@@ -0,0 +1,19 @@
+/**************************************************************************//**
+*
+* \file        Subset_Sum.c
+*
+* \defgroup    Subset_Sum    Generic module to represent a subset sum problem
+*
+* \details     This module is intended to provide a generic framework for
+*              processing an instance of a subset sum problem. The values and
+*              best solution are tracked, along with a pointer to a solver
+*              function that can be changed as needed.
+*
+*              Also included are some generic printing functions for reporting
+*              data.
+*
+* \version     01/21/17  gcg  Initial version.
+*
+* \{
+*
+******************************************************************************/
---
+++ Abstraction/Subset_Sum.h
@@ -0,0 +1,59 @@
+/**************************************************************************//**
+*
+* \file        Subset_Sum.h
+*
+* \version     01/21/16  gcg  Initial version.
+*
+******************************************************************************/
+
+#ifndef _SUBSUM_H
+#define _SUBSUM_H
+
+// ***** Header files *********************************************************
+
+// Basic types
+
+#include <stdint.h>
+
+// ***** Definitions **********************************************************
+
+//! The Subset_Sum_t typedef exists just to hide the void pointer from the
+//! higher level application
+
+typedef void * Subset_Sum_t;
+
+//! The solver function and macro are used to easily create and provide
+//! solution functions to the solver.
+
+typedef void (*Algorithm_t)(void ** zpapvArgs);
+
+#define SUBSETSUM_ALGORITHM(Func)       void (Func) (void ** zpapvArgs)
+
+// ***** Function prototypes **************************************************
+
+// Initialization functions
+
+void Subset_Sum_Initialize (Subset_Sum_t zptHandle);
+
+// Control functions
+
+void Subset_Sum_Solve (Subset_Sum_t zptHandle)
+
+// Input functions
+
+void Subset_Sum_ReadFromFile (Subset_Sum_t zptHandle, char * zpsFilePath);
+void Subset_Sum_ReadFromArray (Subset_Sum_t zptHandle, uint32_t * zpauwData);
+
+void Subset_Sum_SetOutputFile (Subset_Sum_t zptHandle, char * zpsFilePath);
+void Subset_Sum_SetSolver (Subset_Sum_t zptHandle, Algorithm_t ztSolver);
+
+// Print Functions
+
+void Subset_SumDisplayData (Subset_Sum_t zptHandle);
+void Subset_SumWriteData (Subset_Sum_t zptHandle);
+
+// Cleanup functions
+
+void Subset_Sum_Free (Subset_Sum_t zptHandle);
+
+#endif _SUBSUM_H
